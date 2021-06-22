7a73ae7372184ea9a51c6c39bc2ebf6fff0d4632
unknown <tarik.pasalic777@gmail.com>
2021-06-22 16:04:53 +0200

    functionalities

--- ttt/src/components/Board.js
+++ ttt/src/components/Board.js
@@ -1,24 +1,29 @@
-const Board =()=>{
-    
+import React from "react";
+import Square from "./Square";
 
-    const squares = [];
-  
-    for (let i=0;i<9;i++) {
-      squares.push(<div className="square" key={i}>X</div>)
-    }
+const style = {
+  border: "4px solid darkblue",
+  borderRadius: "10px",
+  width: "250px",
+  height: "250px",
+  margin: "0 auto",
+  display: "grid",
+  gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
 
-    return(
-        <div className="container">
-            <div className="container2"> 
-            {squares}
-            </div>
-           
-          
-        
-        </div>
-    )
-   
-   
-   }
-   
-   export default Board;
+function Board(props) {
+  const { squares, onClick } = props;
+  return (
+    <div style={style}>
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)} />
+      ))}
+    </div>
+  );
+}
+
+export default Board;
+
+  /*  for (let i=0;i<9;i++) {
+      squares.push(<div className="square" onClick={()=>onClick(i)} key={i}>X</div>)
+    } {squares}*/
--- ttt/src/components/Game.js
+++ ttt/src/components/Game.js
@@ -1,14 +1,69 @@
 
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
-const Game =()=>{
-    return(
-        <div>
-            <Board/>
-        </div>
-    )
-   
-   
-   }
-   
-   export default Game;
+
+
+const style = {
+  width: "200px",
+  margin: "20px auto",
+};
+
+function Game() {
+
+    const calculateWinner=(squares)=> {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+        }
+        return null;
+      }
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    if (winner || boardCopy[i]) {
+      return;
+    }
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  };
+
+  const renderMoves = () => {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => setBoard(Array(9).fill(null))}>
+          Reset Game
+        </button>
+      </div>
+    );
+  };
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <div style={style}>
+        <p style={{ textAlign: "center" }}>
+          {winner
+            ? `Winner: ` + winner
+            : `Next Player: ` + (xIsNext ? "X" : "O")}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
+}
+
+export default Game;
